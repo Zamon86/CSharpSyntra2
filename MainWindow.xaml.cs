05224a3d8e9438efdc7f6da5de425637e62cea0a
Piotr <zamonagh@gmail.com>
2024-10-15 19:55:53 +0200

    gitignore

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ExtraLogikaOefening
 {
@@ -21,41 +13,114 @@ namespace ExtraLogikaOefening
             InitializeComponent();
         }
 
-        private void btnVulDeWaarde_Click(object sender, RoutedEventArgs e)
+        private double _lidgeldBasis = 100d;
+        private int _ouderdom = 0;
+        private int _kortingOuderdomBoven50 = 0;
+        private int _kortingOuderdomOnder18 = 0;
+        private int _kortingStempel = 0;
+        private int _kortingsBon = 0;
+        private int _kortingGeslacht = 0;
+
+        private void btnVulDeWaarden_Click(object sender, RoutedEventArgs e)
         {
             Random random = new Random();
             txtOuderdom.Text = random.Next(10, 100).ToString();
+
+            if (random.Next(2) == 1)
+            {
+                radJa.IsChecked = true;
+            }
+            else
+            {
+                radNee.IsChecked = true;
+            }
+
+            switch (random.Next(4))
+            {
+                case 0:
+                    radGeenKorting.IsChecked = true;
+                    break;
+
+                case 1:
+                    radKorting10.IsChecked = true;
+                    break;
+
+                case 2:
+                    radKorting20.IsChecked = true;
+                    break;
+
+                case 3:
+                    radKorting25.IsChecked = true;
+                    break;
+            }
+
+            if (random.Next(2) == 1)
+            {
+                radVrouw.IsChecked = true;
+            }
+            else
+            {
+                radMan.IsChecked = true;
+            }
+
         }
 
         private void btnBereken1_Click(object sender, RoutedEventArgs e)
         {
-            double lidgeldBasis = 100d;
-            int ouderdom;
-
-            if (string.IsNullOrEmpty(txtOuderdom.Text))
+            if (!CheckGegevens())
             {
-                MessageBox.Show("Vul je ouderdom in!");
-                return;
-            } 
-            else if (!Int32.TryParse(txtOuderdom.Text, out ouderdom) || ouderdom > 120)
-            {
-                MessageBox.Show("Ouderdom moet een geheel getal zijn tussen 5 en 120");
                 return;
             }
 
-            int kortingOuderdomBoven50 = 0;
-            int kortingOuderdomOnder18 = 0;
-            int kortingStempel = 0;
-            int kortingsBon = 0;
-            int kortingGeslacht = 0;
+            SetStandaardKortingen();
+            CalculateLidgeld();            
+        }
 
-            if (ouderdom < 18)
+        
+
+        private void btnBereken2_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckGegevens())
             {
-                kortingOuderdomOnder18 = 50;
+                return;
             }
-            else if (ouderdom > 50)
+
+            SetStandaardKortingen();
+            
+            if (_kortingOuderdomBoven50 > 0)
             {
-                kortingOuderdomBoven50 = ouderdom - 50;
+                int somSpeciaalKorting = 0;
+                for (int i = 1; i <= _kortingOuderdomBoven50; i++)
+                {
+                    somSpeciaalKorting += i;
+                }
+                _kortingOuderdomBoven50 = somSpeciaalKorting;
+            }
+            CalculateLidgeld();
+        }
+
+        private void CalculateLidgeld()
+        {
+            int somVanProcentueleKortingen = _kortingOuderdomOnder18 + _kortingsBon;
+            double lidgeld = (_lidgeldBasis - _kortingStempel - _kortingGeslacht - _kortingOuderdomBoven50) * ((100d - somVanProcentueleKortingen) / 100);
+            if (lidgeld < 0)
+            {
+                lidgeld = 0;
+            }
+
+            ResetVariables();
+            MessageBox.Show("Lidgeld is " + Math.Round(lidgeld, 2).ToString("F2") + " €.");
+        }
+
+        private void SetStandaardKortingen()
+        {
+            if (_ouderdom < 18)
+            {
+                _kortingOuderdomOnder18 = 50;
+            }
+            else if (_ouderdom > 50)
+            {
+                _kortingOuderdomBoven50 = _ouderdom - 50;
             }
 
             RadioButton selectedRadioButton1 = SelectedRadioButton(spStempel);
@@ -65,7 +130,7 @@ namespace ExtraLogikaOefening
             switch (selectedRadioButton1.Content.ToString())
             {
                 case "Ja":
-                    kortingStempel = 25;
+                    _kortingStempel = 25;
                     break;
 
                 case "Nee":
@@ -78,15 +143,15 @@ namespace ExtraLogikaOefening
             switch (selectedRadioButton2.Content.ToString())
             {
                 case "10%":
-                    kortingsBon = 10;
+                    _kortingsBon = 10;
                     break;
 
                 case "20%":
-                    kortingsBon = 20;
+                    _kortingsBon = 20;
                     break;
 
                 case "25%":
-                    kortingsBon = 25;
+                    _kortingsBon = 25;
                     break;
 
                 default:
@@ -96,20 +161,38 @@ namespace ExtraLogikaOefening
             switch (selectedRadioButton3.Content.ToString())
             {
                 case "Vrouw":
-                    kortingGeslacht = 15;
-                    break;                
+                    _kortingGeslacht = 15;
+                    break;
 
                 default:
                     break;
             }
+        }
 
-            int somVanProcentueleKortingen = kortingOuderdomOnder18 + kortingsBon;
-            double lidgeld = (lidgeldBasis - kortingStempel - kortingGeslacht - kortingOuderdomBoven50) * ((100d - somVanProcentueleKortingen) / 100);
-            if (lidgeld < 0)
+        private void ResetVariables()
+        {
+            _ouderdom = 0;
+            _kortingOuderdomBoven50 = 0;
+            _kortingOuderdomOnder18 = 0;
+            _kortingStempel = 0;
+            _kortingsBon = 0;
+            _kortingGeslacht = 0;
+        }
+
+        private bool CheckGegevens()
+        {
+            if (string.IsNullOrEmpty(txtOuderdom.Text))
             {
-                lidgeld = 0;
+                MessageBox.Show("Vul je _ouderdom in!");
+                return false;
             }
-            MessageBox.Show("Lidgeld is " + lidgeld);
+            else if (!Int32.TryParse(txtOuderdom.Text, out _ouderdom) || _ouderdom > 120)
+            {
+                MessageBox.Show("Ouderdom moet een geheel getal zijn tussen 5 en 120");
+                return false;
+            };
+
+            return true;
         }
 
         private RadioButton SelectedRadioButton(StackPanel spStempel)
@@ -119,12 +202,12 @@ namespace ExtraLogikaOefening
                 if (rb.IsChecked == true)
                 {
                     return rb;
-                } 
+                }
             }
 
             RadioButton noRadioButtonSelected = new RadioButton();
             noRadioButtonSelected.Content = "No radiobutton selected";
             return noRadioButtonSelected;
-        }
+        }        
     }
 }
